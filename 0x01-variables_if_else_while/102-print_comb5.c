f465d0173edca06dca363e7b13d82707efb5a9f5
Devas <vardevvas@gmail.com>
2023-02-18 13:43:11 +0200

    29

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -23,15 +23,15 @@ int main(void)
 					op2 = (e * 10) + i;
 					if (op1 < op2)
 					{
-						putchar(h);
-						putchar(g);
-						putchar(' ');
-						putchar(e);
-						putchar(i);
-						if (h == 57 && g == 56 && e == 57 && i == 57)
-							break;
-						putchar(',');
-						putchar(' ');
+					putchar(h);
+					putchar(g);
+					putchar(' ');
+					putchar(e);
+					putchar(i);
+					if (h == 57 && g == 56 && e == 57 && i == 57)
+					break;
+					putchar(',');
+					putchar(' ');
 					}
 					i++;
 				}
