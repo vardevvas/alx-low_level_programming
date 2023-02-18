d25fa8ef279b15366fa260bec2175f7518c87e9c
Devas <vardevvas@gmail.com>
2023-02-18 11:31:13 +0200

    3

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,10 +15,10 @@ l = n % 10;
 if (l > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, l);
-}
+
 else if (l == 0)
 {
-printf("Last digit of %d is %d and is 0\n", n, l);
+printf("%d is zero\n", n, l);
 }
 else
 {
