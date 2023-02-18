e083b02a9303b1efebd281f211f0629999c28284
Devas <vardevvas@gmail.com>
2023-02-18 11:32:30 +0200

    4

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,21 +8,21 @@
  */
 int main(void)
 {
-int n, l;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-l = n % 10;
-if (l > 5)
-{
-printf("Last digit of %d is %d and is greater than 5\n", n, l);
-}
-else if (l == 0)
-{
-printf("%d is zero\n", n, l);
-}
-else
-{
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
-}
-return (0);
+    int n, l;
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+    l = n % 10;
+    if (l > 5)
+    {
+        printf("Last digit of %d is %d and is greater than 5\n", n, l);
+    }
+    else if (l == 0)
+    {
+        printf("%d is zero\n", n, l);
+    }
+    else
+    {
+        printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+    }
+    return (0);
 }
