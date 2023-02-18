c820b97cc5cfe4b5b3f925402a965d88049bf803
Devas <vardevvas@gmail.com>
2023-02-18 13:17:16 +0200

    24

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/** * main - Prints numbers between 012 to 789. * * Return: Always 0 (Success) */ 
+/** 
+ * main - Prints numbers between 012 to 789.
+ *
+ * Return: Always 0 (Success) 
+ */ 
 int main(void)
 {
 	int i, e, g;
