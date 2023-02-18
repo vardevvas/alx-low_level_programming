4d719ad0a4ff337f20900a6a8c2e36fd69de819d
Devas <vardevvas@gmail.com>
2023-02-18 12:03:15 +0200

    16

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,13 +6,12 @@
  */
 int main(void)
 {
-	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
-	for (i = 0; i < 52; i++)
-	{
-		putchar(alp[i]);
-	}
-	putchar('\n');
-	return (0);
+    for (i = 0; i < 52; i++)
+    {
+	    putchar(i);
+    }
+    putchar('\n');
+    return (0);
 }
