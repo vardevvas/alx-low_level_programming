814d4c432b4c2439123e94a57287853977739e38
Ali <vardevvas@gmail.com>
2023-02-22 14:54:10 +0200

    5

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,11 +7,13 @@
 void print_alphabet_x10(void)
 {
     char i;
-    for(i = 1; i <=10; i++){
-    for (i = 'a'; i <= 'z'; i++)
-    {
-        _putchar(i);
-    }
-    _putchar('\n');
+    int j;
+
+    for (j = 0; j<10; j++){
+	    for (i = 'a'; i <= 'z'; i++)
+	    {
+		    _putchar(i);
+	    }
+	    _putchar('\n');
     }
 }
